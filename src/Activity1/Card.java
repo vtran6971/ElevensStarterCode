6934c13ea8281372eed4fc4e22406a690daee7cd
Vincent Tran <vtran6971@bths.edu>
2019-02-13 22:40:46 -0500

    All getmethods, stringTO, and compareto method completed and added cannont run main due to errors in other packages :\

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -35,6 +35,9 @@ public class Card {
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        this.rank = cardRank;
+        this.suit = cardSuit;
+        this.pointValue = cardPointValue;
     }
 
 
@@ -44,7 +47,7 @@ public class Card {
      */
     public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+return suit;
     }
 
     /**
@@ -53,7 +56,7 @@ public class Card {
      */
     public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+return rank;
     }
 
     /**
@@ -62,7 +65,7 @@ public class Card {
      */
     public int pointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+return pointValue;
     }
 
     /** Compare this card with the argument.
@@ -73,7 +76,12 @@ public class Card {
      */
     public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        boolean bool = false;
+     if(rank.compareTo(otherCard.rank) == 0 && suit.compareTo(otherCard.suit) == 0)
+      {
+          bool = true;
+      }
+     return bool;
     }
 
     /**
@@ -89,7 +97,10 @@ public class Card {
     @Override
     public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+     String format = "";
 
+     format = rank() +" of " + suit() + " point value = " + pointValue();
+     return format;
     }
 }
 
--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,17 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String[] args) {
+
+
+    //Creation of cards
+    Card heartTen = new Card("ten", "heart", 10);
+    Card clubSix = new Card("six", "club", 6);
+    Card spadeAce = new Card("ace", "spade", 1);
+
+    System.out.println("rank is "+heartTen.rank()+ "suit is " +heartTen.suit()+ "point value is "+ heartTen.pointValue() );
+        System.out.println("rank is " + "suit is " + "point value is " );
+        System.out.println("rank is " + "suit is " + "point value is " );
+
+}
 }
